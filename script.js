258474b73e305059adf11f3ecc021166a816a099
sifa <sifap122@gmail.com>
2024-12-03 23:20:56 +0530

    spread operator

--- script.js
+++ script.js
@@ -321,3 +321,22 @@ const book1 = {
 printBookInfo(book1);
 
 printBookInfo({ title: "Let us C", author: "Yashwant Kanetakar" });
+
+//Spread Operator
+
+// 3.1
+// Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+
+// Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
+
+const bookAuthors = [...books[0].author, ...books[1].author];
+console.log(bookAuthors);
+
+//3.2
+//Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+
+function spellWord(arr) {
+  console.log(...arr);
+}
+
+spellWord("javascript");
