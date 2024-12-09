9b20a69e8ae4ee7b435fda689ea7f7547a1ee6bb
sifa <sifap122@gmail.com>
2024-12-09 21:43:06 +0530

    assig 4 Rest Operator and Spread Operator

--- script.js
+++ script.js
@@ -340,3 +340,33 @@ function spellWord(arr) {
 }
 
 spellWord("javascript");
+
+//Rest Operator and Spread Operator
+
+//4.1
+//Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
+
+const [mainKeyword, ...rest] = books[0].keywords;
+console.log(mainKeyword, rest);
+
+//4.2
+//Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
+
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+console.log(bookPublisher, restOfTheBook);
+
+//4.3
+//Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
+
+printBookAuthorsCount(
+  books[1].title,
+  "Harold Abelson",
+  "Gerald Jay Sussman",
+  "Julie Sussman (Contributor)"
+);
