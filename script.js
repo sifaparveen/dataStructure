785e6d78ba328e180b690f5706493b7f4342323b
sifa <sifap122@gmail.com>
2024-12-10 20:59:41 +0530

    for of loop

--- script.js
+++ script.js
@@ -418,3 +418,34 @@ console.log(books[books.length - 2].edition);
 for (let i = 0; i < books.length; i++) {
   books[i].highlighted = books[i].thirdParty.goodreads.rating < 4.2 && "false";
 }
+
+// 8) Looping Arrays: The for-of Loop
+
+//8.1
+//Use the for-of loop to loop over the books array and sum the pages of all books. Use the pageSum variable below, and the pages property of the book objects.
+let pageSum = 0;
+for (let book of books) {
+  pageSum += book.pages;
+}
+console.log(pageSum);
+
+//8.2
+//Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
+//Remember that each book object has the author property, which can be a string (if there is only a single author) or an array (if there are multiple authors). You may need to use the typeof operator. You can also use multiple loops if needed. The allAuthors array should have just one level (no nested arrays).
+
+const allAuthors = [];
+
+for (let book of books) {
+  if (typeof book.author === "object") {
+    for (let name of book.author) allAuthors[allAuthors.length] = name;
+  } else allAuthors[allAuthors.length] = book.author;
+}
+
+console.log(allAuthors);
+
+//8.3
+//Use the for-of loop together with Array's entries() method to log each author from allAuthors to the console together with its index. Make the index start from 1, instead of 0.
+
+for (let [index, author] of allAuthors.entries()) {
+  console.log(`${index + 1}. ${author}`);
+}
